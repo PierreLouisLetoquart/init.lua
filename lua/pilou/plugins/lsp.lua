b312b040d02cbce25220f40c3d399d8c12cb4149
pletoquart <pletoquart@hexaly.com>
2026-01-28 10:03:31 +0100

    minor update

--- lua/pilou/plugins/lsp.lua
+++ lua/pilou/plugins/lsp.lua
@@ -5,8 +5,10 @@ return {
 		dependencies = { "neovim/nvim-lspconfig" },
 		opts = {
 			ensure_installed = {
+				"html",
 				"ruff",
 				"pyright",
+				"prettier",
 				"rust_analyzer",
 				"lua_ls",
 				"biome",
