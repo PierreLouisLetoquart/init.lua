ebf4f8ec6534abe472fc5f1d29afbe53465a565c
pletoquart <pletoquart@hexaly.com>
2026-02-05 12:17:12 +0100

    __pycache__ hidden by default

--- lua/pilou/plugins/oil.lua
+++ lua/pilou/plugins/oil.lua
@@ -8,6 +8,12 @@ return {
 		},
 		delete_to_trash = true,
 		skip_confirm_for_simple_edits = true,
+		view_options = {
+			is_hidden_file = function(name, bufnr)
+				local m = name:match("^%.") or name:match("__pycache__$")
+				return m ~= nil
+			end,
+		},
 	},
 	dependencies = { { "echasnovski/mini.icons", opts = {} } },
 	lazy = false,
