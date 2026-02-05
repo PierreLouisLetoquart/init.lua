64b93186053aff4d7efa4b69ed0aba58769a3b8b
pletoquart <pletoquart@hexaly.com>
2026-02-05 09:50:43 +0100

    sync taff

--- lua/pilou/plugins/treesiter.lua
+++ lua/pilou/plugins/treesiter.lua
@@ -1,7 +1,7 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	main = "nvim-treesitter.config",
+	main = "nvim-treesitter.configs",
 	opts = {
 		ensure_installed = {
 			"bash",
